57401129456323240632f65872b8a33a358cf578
Rawand Dargali <rdargali@yahoo.com>
2019-10-21 10:58:57 -0500

    final template commit

--- app.js
+++ app.js
@@ -5,5 +5,25 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const accountRouter = require('./routes/accounts');
 
+app.set("view engine", "pug");
+
+app.get("/",  (req, res) => {
+
+});
+
+app.get("/login", (req, res) => {
+    res.render('login')
+});
+
+
+app.get("/register", (req, res) => {
+    res.render('register')
+});
+
+
+
+
+
+app.listen(3000)
 
 
