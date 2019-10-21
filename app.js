f5b040851d4b63e373d5ed21d65f0253d28ebef9
Rawand Dargali <rdargali@yahoo.com>
2019-10-21 15:12:44 -0500

    changes to account and register

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const accountRouter = require('./routes/accounts');
 
+const users = [
+  {id: 1, name: 'Rawand', email: 'rawand@gmail.com', password: 'password'},
+  {id: 2, name: 'Huy', email: 'huy@gmail.com', password: 'password'},
+  {id: 3, name: 'Steven', email: 'steven@gmail.com', password: 'password'},
+  {id: 4, name: 'Angelo', email: 'angelo@gmail.com', password: 'password'}
+]
+
 app.use(
   session({
     secret: "the new twitter",
@@ -18,16 +25,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
+  
   res.render("index");
+  const {userID} = req.session
+  console.log(userID)
 });
 
 app.get("/login", (req, res) => {
-    res.render('login');
+  res.render('login');
+  //req.session.userID
+});
+
+app.get("/account", (req, res) => {
+  res.render('account');
+  
 });
 
 
 app.get("/register", (req, res) => {
+  res.render('register');
+});
+
+
+
+app.get("/register", (req, res) => {
     res.render('register');
+});
+
+
+
+app.post("/login", (req, res) => {
+  res.render('login');
+  
+});
+
+
+app.post("/register", (req, res) => {
+  res.render('register');
+});
+
+app.post("/logout", (req, res) => {
+  res.render('register');
 });
 
 
